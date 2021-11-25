93c609b19d127160b0e5b5fb467519a88fd6b8fd
lizeshakya <lizeshakya@gmail.com>
2021-11-25 16:41:41 +0545

    String tuts complete

---
+++ 4_string.py
@@ -0,0 +1,28 @@
+# course = 'Python for "Beginners"'
+# print(course)
+#
+# course_episode = "This is one's favourite episode"
+# print(course_episode)
+#
+# support_ticket = """
+# Hello John,
+#
+# "This is our first email's to you"
+#
+# Thank you,
+# Support Team
+# """
+#
+# print(support_ticket)
+
+# course = "String in Python"
+# print(course[0])
+# print(course[-2])
+# print(course[0:3])
+# print(course[2:])
+# print(course[:5])
+# print(course[:])
+# print(course)
+
+name = 'Jennifer'
+print(name[1:-1]) # enneife
