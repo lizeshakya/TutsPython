eb736712b298be2a81c7a7a689595e7ba856a86d
lizeshakya <lizeshakya@gmail.com>
2021-11-25 16:01:40 +0545

    Receiving input complete

---
+++ 1_variables.py
@@ -0,0 +1,4 @@
+# Variable Exercise 1
+name = 'John Smith'
+age = 20
+is_new = True
---
+++ 2_receiving_input.py
@@ -0,0 +1,8 @@
+# Tutorial
+# name = input("What is your name? ")
+# print("Hi " + name)
+
+# Exercise
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+print(name + ' likes ' + color)
