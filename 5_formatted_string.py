8d2d73c5bfc3cca8e7f6961335c030c46a2e71e2
lizeshakya <lizeshakya@gmail.com>
2021-12-01 09:57:26 +0545

    Committed the format string

--- 5_formatted_string.py
+++ 5_formatted_string.py
@@ -2,5 +2,7 @@ first_name = 'John'
 last_name = 'Smith'
 # Need to print: John[Smith] is a coder
 message = first_name + '[' + last_name + '] is a coder'
+msg = f'{first_name} [{last_name}] is a coder'
 print(message)
+print(msg)
 
