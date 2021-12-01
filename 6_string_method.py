249aba28ce202e2f2fc6505ca8b053d8953a305b
lizeshakya <lizeshakya@gmail.com>
2021-12-01 10:09:59 +0545

    Learned methods like: lower, upper, title, find, replace, and in

---
+++ 6_string_method.py
@@ -0,0 +1,16 @@
+course = 'Python Learning with Lizesh'
+# print(len(course))
+
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+
+# print(course.find('P'))
+# print(course.find('yt'))
+# print(course.find('asd')) #returns -1
+# print(course.replace('Learning', 'Expertise'))
+# print(course.replace('testing', 'Expertise')) # returns original
+# print(course.replace('n', 'a')) # Changes all n to a
+
+# print('Pything' in course) # Returns False
+# print('Python' in course) # Returns True
